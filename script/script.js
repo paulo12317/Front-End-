dcdd81b8aebd0f0b7042c5f5a650737a46c2c005
PAULO HENRIQUE VICENTE FRIEDRICH <04926466007@senacrs.edu.br>
2025-05-02 17:26:30 -0300

    Aula 17

---
+++ script/script.js
@@ -0,0 +1,12 @@
+// Init constellation plugin
+$('canvas').constellation({
+    star: {
+      color: '#fff',
+      width: 3
+    },
+      line: {
+      color: '#fff'
+    },
+    radius: 180,
+    density: 1280.0 * 640.0 / 200.0
+  })
